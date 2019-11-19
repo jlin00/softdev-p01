c7ecdb49669369198e6b294a865dc636ed3bdc80
Amanda Zheng <azheng00@stuy.edu>
2019-11-19 08:13:30 -0500

    begin routes for login and signup

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+#Amanda Zheng
+#SoftDev1 pd1
+#K25: Getting More Rest
+#2019-11-13
+from flask import Flask , render_template
+import urllib, json
+from json import loads
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+@app.route("/login")
+def login():
+
+@app.route("/signup")
+def signup():
+
+
 
 if __name__ == "__main__":
     app.debug = True
