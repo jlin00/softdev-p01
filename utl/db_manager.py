b662dec603cb14f750a94d4a71d53ef2fea2ffad
Jackie Lin <jlin00@stuy.edu>
2019-11-19 08:38:12 -0500

    jackieL: wrote new addUser function in db_manager

---
+++ utl/db_manager.py
@@ -0,0 +1,17 @@
+#Team Kirkland Meeseeks
+#SoftDev pd1
+#P01 -- ArRESTed Development
+#2019-1?-??
+
+import sqlite3
+from utl.db_builder import exec
+
+#add user-provided credentials to database
+def addUser(username, password, flag):
+    q = "SELECT * FROM user_tbl WHERE username = '%s';" % username
+    data = exec(q).fetchone()
+    if (data is None):
+        q = "INSERT INTO user_tbl VALUES('%s', '%s', '', '', '', 200, '%s', '', 0)" % (username, password, flag)
+        exec(q)
+        return True
+    return False #if username already exists
